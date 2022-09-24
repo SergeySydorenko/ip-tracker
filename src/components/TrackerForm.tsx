83bc5a2396318d495ae773b235cd258feccfdd9d
Sergey <75169619+SergeySydorenko@users.noreply.github.com>
2022-09-24 22:02:09 +0300

    tracker form component + design

---
+++ src/components/TrackerForm.tsx
@@ -0,0 +1,13 @@
+
+export default function TrackerForm(){
+
+    return(
+        <section className="tracker">
+            <h1>IP Address Tracker</h1>
+            <form className="tracker-form" onSubmit={e => e.preventDefault()}>
+                <input placeholder="Search for any IP address or domain"></input>
+                <button type="submit">{'>'}</button>
+            </form>
+        </section>
+    )
+}
